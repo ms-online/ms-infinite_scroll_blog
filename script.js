028c0a7fe6570ceac4084bea2eb0998e938d9f62
ms-online <27732357@qq.com>
2020-03-17 14:32:30 +0800

    lesson-5

--- script.js
+++ script.js
@@ -36,5 +36,27 @@ async function showPosts() {
   });
 }
 
-// 初始化页面
+//showLoading
+function showLoading() {
+  loading.classList.add("show");
+  setTimeout(() => {
+    loading.classList.remove("show");
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+// 显示页面帖子
 showPosts();
+
+// 事件监听
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
